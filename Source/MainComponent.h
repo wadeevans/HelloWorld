51fe305d31f916039e403bc779f163b1e1e6e23b
Chris Wade-Evans <wevans@cix.co.uk>
2021-06-07 17:35:57 +0100

    Defined a Car and Car Seat type

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -2,6 +2,23 @@
 
 #include <JuceHeader.h>
 
+struct Car
+{
+    struct CarSeat
+    {
+        bool seatIsLeather;
+    };
+    
+    int numberOfWheels;
+    bool convertible;
+    CarSeat driversSeat;
+    CarSeat navigatorsSeat;
+    
+    bool switchSeats(CarSeat oldSeat, CarSeat newSeat);
+    
+    void accelerate(float howFarToPushThePedal);
+};
+
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
